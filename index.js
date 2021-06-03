5183e1f98573e5c7a292c50ccae387d216b4cae5
V-Ghost <vukaniabryan@gmail.com>
2021-06-03 15:49:05 +0000

    a

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 require('dotenv').config()
+const axios = require('axios');
 var express = require('express')
 var app = express()
 var port = 3000
-// respond with "hello world" when a GET request is made to the homepage
+let config = {
+  headers: {
+    Authorization: 'Bearer sk_test_fda84a0251f0521e87cea8b0a675ccd4d6c74269',
+    'Content-Type': 'application/json'
+  }
+}
+
+let params = JSON.stringify({
+  "email": "vukaniabryan@email.com",
+  "amount": "20000",
+  
+})
+// respond with "hello world" when a GET request sis made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+  return res.send(process.env.PORT)
+  // axios.post('https://api.paystack.co/transaction/initialize', params, config)
+  // .then(feedback => {
+  //   // console.log(`statusCode: ${res.statusCode}`)
+  //   console.log(feedback.data)
+  //   res.send("streets be cold")
+  // })
+  // .catch(error => {
+  //   console.log(error)
+  //   res.send("error")
+  // })
+})
+
+app.post('/callback', (req, res) => {
+  console.log(req.body)
+  return res.end()
 })
 
 
