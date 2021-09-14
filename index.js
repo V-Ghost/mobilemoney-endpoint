8490b445129ba660f84ca8fa275a168f090a3524
V-Ghost <vukaniabryan@gmail.com>
2021-09-14 18:11:38 +0000

    update

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ var defaultAuth = fs.auth();
 //   "amount": "20000",
   
 // })
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 app.get('/tag/:tagId',async function  (req, res)  {
   var userId ;
   
